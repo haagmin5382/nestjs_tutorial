6baf5e611f44dae59a9f3c1cf57f29976e2fb9d4
haagmin5382 <81218474+haagmin5382@users.noreply.github.com>
2024-03-13 19:48:40 +0900

    ✨ feat : add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config'; // .env 사용
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    // 모듈 사용
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI), // db 연결
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService], // 제거하면 공급자가 없음 그래서 controller(소비자)는 제품(공급자, Service)을 사용할 수 없음
 })
